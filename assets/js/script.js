d8b903a2fd71870658fa4368da2689921e1f4017
Evolan <yon_baermund@hotmail.com>
2022-11-04 22:49:48 -0400

    got the questions to appear on the website while having the timer running.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,41 @@
 //this is my timer function variables.
 var timeEl = document.querySelector(".time");
 var startGame = document.querySelector("#start");
-//this are my questions
+// //this is the timer and its variable for time, 
+var secondsLeft = 60;
+
+function clockDown() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+        
+        if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        window.alert("Sorry but you lost!!!");
+    }
+    }, 1000);
+    
+}
+
+//this are the variables to call on all the quiz elements to the website
+var question = document.getElementById("question");
+var choice1 = document.getElementById("A");
+var choice2 = document.getElementById("B");
+var choice3 = document.getElementById("C");
+var choice4 = document.getElementById("D");
+
+//this are the questions
 let quest = [
 {
-    question1: "What is the Name of the Anime's main Character?>",
-    choice1: "Toshinori Yagi a.k.a. All Might",
-    choice2: "Izuku Midoriya a.k.a. Deku",
-    choice3: "Enji Todoroki a.k.a. Endeavor",
-    choice4: "Tenko Shimura a.k.a. Shigaraki",
+    question: "What is the Name of the Anime's main Character?",
+    choice1: "Toshinori Yagi  a.k.a. - All Might -",
+    choice2: "Izuku Midoriya a.k.a. -Deku-",
+    choice3: "Enji Todoroki a.k.a. -Endeavor-",
+    choice4: "Tenko Shimura a.k.a. -Shigaraki-",
     correct: "choice2"
 },
 {
-    question2: "Who inherited All Might's superpower?",
+    question: "Who inherited All Might's superpower?",
     choice1: "Deku",
     choice2: "Bakugo",
     choice3: "Todoroki",
@@ -20,7 +43,7 @@ let quest = [
     correct: "choice1"
 },
 {
-    question3: "Who becomes the number-one hero after All might retired?",
+    question: "Who becomes the number-one hero after All might retired?",
     choice1: "Hawks",
     choice2: "Dynamight",
     choice3: "Deku",
@@ -28,7 +51,7 @@ let quest = [
     correct: "choice4"
 },
 {
-    question4: "What is Bakugo's a.k.a.Dynamight superpower?",
+    question: "What is Bakugo's a.k.a.Dynamight superpower?",
     choice1: "Insults everytone to death",
     choice2: "Can fly at superspeed",
     choice3: "Shoots Lasers from his nostrils",
@@ -36,38 +59,40 @@ let quest = [
     correct: "choice4"
 },
 {
-    question5: "Where do the aspiring superheroes train to become professionals?",
+    question: "Where do the aspiring superheroes train to become professionals?",
     choice1: "They receiva a letter welcoming them to Hero-gwartz",
     choice2: "They all pay top dollar for Superschool",
     choice3: "They apply to U.A. Academy",
     choice4: "They take a 24 week online bootcamp where they get their license",
     correct: "choice3"
-}
-];
+}];
 
-var secondsLeft = 60;
+//this will prompt the questions when the start button is clicked.
+var quizStart = quest.length-1;
+let currentQuestion = 0;
 
-function clockDown() {
-    var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft;
-
-    if(secondsLeft === 0) {
-    clearInterval(timerInterval);
-    window.alert("Sorry but you lost!!!");
-    }
-    }, 1000);
-    
+function askThis (){
+    let trivia = quest[currentQuestion];
+    question.innerHTML =trivia.question;
+    choice1.innerHTML = trivia.choice1;
+    choice2.innerHTML = trivia.choice2;
+    choice3.innerHTML = trivia.choice3;
+    choice4.innerHTML = trivia.choice4;
 }
 
-//timer works! needs fixing for a restart
+
+
+
 
 //this function will start the game
 startGame.addEventListener("click", function() {
-//this starts the timer countdown while reseting it    
+    //this starts the timer countdown while reseting it    
     clockDown();
-//this prompts the questions    
+    askThis(); 
 });
+
+
+
 
 
 
